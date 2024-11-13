d6e820687bb40aff51877655e706b39d992b49ac
Chloe <chloe.martins@etec.sp.gov.br>
2024-11-13 18:26:54 -0300

    Não vou escrever uma mensagem útil.

--- res/js/index.js
+++ res/js/index.js
@@ -1,9 +1,13 @@
-document.getElementById('cpf').addEventListener('input', function(e) {
-    var value = e.target.value;
-    var cpfPattern = value.replace(/\D/g, '')
-                          .replace(/(\d{3})(\d)/, '$1.$2')
-                          .replace(/(\d{3})(\d)/, '$1.$2')
-                          .replace(/(\d{3})(\d)/, '$1-$2')
-                          .replace(/(-\d{2})\d+?$/, '$1');
-    e.target.value = cpfPattern;
-});
+
+function cpf(input) {
+    // Remover todos os valores que não são números
+    let value = input.value.replace(/\D/g, '');
+
+    // Formatação
+    if (value.length > 3) value = value.slice(0, 3) + '.' + value.slice(3);
+    if (value.length > 6) value = value.slice(0, 7) + '.' + value.slice(7);
+    if (value.length > 9) value = value.slice(0, 11) + '-' + value.slice(11);
+
+    // Limita o input a 14 caracteres (incluindo os pontos e traço)
+    input.value = value.slice(0, 14);
+}
