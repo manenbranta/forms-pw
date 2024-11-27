3831dfa4f8cd6a9ba3d8bda494c003669a69d64f
Chloe <chloe.martins@etec.sp.gov.br>
2024-11-27 18:21:00 -0300

    Fix telephone formatting

--- res/js/formatters.js
+++ res/js/formatters.js
@@ -16,7 +16,7 @@ function format_telefone(input) {
 
     // Formatação
     if (value.length > 2) value = `(${value.slice(0,2)}) ${value.slice(2)}`;
-    if (value.length > 9) value = `${value.slice(0,9)}-${value.slice(9)}`;
+    if (value.length > 10) value = `${value.slice(0,10)}-${value.slice(10)}`;
 
     input.value = value;
 }
